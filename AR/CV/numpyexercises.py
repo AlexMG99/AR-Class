7909fd467513d9cdb9e46a4831c02bf14a5a9096
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2020-02-18 09:55:43 +0100

    Working exercises

---
+++ AR/CV/numpyexercises.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+# 1.Create array of 10f zeros
+vector = np.zeros(10)
+print(vector)
+
+# 2.Create array of 10f zeros whose 5th element is 1
+vector = np.zeros(10)
+vector[4] = 1
+print(vector)
+
+# 3.Create vector of int from 10 to 49
+vector = np.arange(10, 50)
+print(vector)
+
+# 4.Create matrix 3x3f from 1 to 9
+vector = np.arange(1, 10)
+vector = vector.reshape((3, 3))
+print(vector)
+
+# 5.Create matrix 3x3 from 1 to 9 and flip horizontally
+vector_fh = np.flip(vector, 1)
+print(vector_fh)
+
+# 6.Create matrix 3x3 from 1 to 9 and flip horizontally
+vector_fv = np.flip(vector, 0)
+print(vector_fv)
+
+# 7.Create 3x3 identity mat
+mat = np.identity(3)
+print(mat)
+
+# 8.Create 3x3 mat rand values
+r_mat = np.random.randint(0, 100, 9)
+r_mat = r_mat.reshape((3,3))
+print(r_mat)
+
+# 9.Create rand vector of 10 num and compute the mean
+r_vec = np.random.randint(0, 20, 10)
+print(r_vec)
+average = r_vec.mean()
+print(average)
+
+# 10.Create 10x10 zero array sourranded by 1
+mat = np.ones((10,10))
+mat[1:9, 1:9] = 0.0
+print(mat)
+
+# 11.Create 5x5 matrix of rows from 1 to 5
+mat = np.zeros((5,5))
+mat[:] = np.arange(1,6)
+print(mat)
+
+# 12.Create array of 9 rand int and reshape to 3x3 mat float
+mat = np.random.seed(101)
+mat = np.random.randint(0, 100, 9)
+mat = mat.reshape((3,3))
+mat = mat.astype(float)
+print(mat)
+
+# 13.Create 5x5 rand mat and substarct average
+r_mat = np.random.randint(0, 100, 25)
+r_mat = r_mat.reshape((5,5))
+print(r_mat)
+average = r_mat.mean()
+print(average)
+
+# 14.Create 5x5 rand mat and substract average of eah row to each row
+r_mat = np.random.randint(0, 100, 25)
+r_mat = r_mat.reshape((5,5))
+print(r_mat)
+
+# 15.Create 5x5 rand mat and return value closer to 0.5
+r_mat = np.random.seed(101)
+r_mat = np.random.random_sample((5,5))
+
+r_mat = np.asarray(r_mat)
+idx = (np.abs(r_mat - 0.5)).argmin()
+print(r_mat)
+print(r_mat[idx])
