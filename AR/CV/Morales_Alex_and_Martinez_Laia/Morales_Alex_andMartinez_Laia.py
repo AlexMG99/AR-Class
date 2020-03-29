1715da6c3c3163be11dbea4602ae3328f6b4f079
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2020-03-29 14:14:15 +0200

    Finsihed

--- AR/CV/Morales_Alex_and_Martinez_Laia/Morales_Alex_andMartinez_Laia.py
+++ AR/CV/Morales_Alex_and_Martinez_Laia/Morales_Alex_andMartinez_Laia.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
 
-def ObjectDetection(inp, tgt):
+def ObjectDetection(input_img, target_img, scale_opt):
+    inp = cv2.resize(input_img, (int(input_img.shape[1] * scale_opt), int(input_img.shape[0] * scale_opt)),  interpolation=cv2.INTER_NEAREST)
+    tgt = cv2.resize(target_img, (int(target_img.shape[1] * scale_opt), int(target_img.shape[0] * scale_opt)), interpolation=cv2.INTER_NEAREST)
     width = (inp.shape[0] - tgt.shape[0] + 1)
     height = (inp.shape[1] - tgt.shape[1] + 1)
     result_img = np.ones((width, height))
@@ -25,22 +27,23 @@ def IsFound(res, threshold):
 
 def main():
     # Image names
-    tgt_name = 't1-img2.png'
-    inp_name = 'img2.png'
+    tgt_name = 't1-img1.png'
+    inp_name = 'img1.png'
+    scale_optimizer = 0.5
 
     # Load image in grayscale
     target_img = cv2.imread(tgt_name, cv2.IMREAD_GRAYSCALE)
     input_img = cv2.imread(inp_name, cv2.IMREAD_GRAYSCALE)
 
     # Detects object
-    result_img = ObjectDetection(input_img, target_img)
+    result_img = ObjectDetection(input_img, target_img, scale_optimizer)
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     # Load image in color
     tgt_img_col = cv2.imread(tgt_name)
     inp_img_col = cv2.imread(inp_name)
     cv2.imshow("Target", tgt_img_col)
-    cv2.imshow("Matching Map", inp_img_col)
+    cv2.imshow("Matching Map", result_img)
 
     # Checks if Target Found
     if(IsFound(result_img, 0.1) == 'FOUND'):
@@ -53,7 +56,7 @@ def main():
     # Creates rectangle of matching icon
     rec_pos = np.where( result_img < 0.1)
     for pixel in zip(*rec_pos[::-1]):
-        cv2.rectangle(inp_img_col, pixel, (pixel[0] + target_img.shape[0], pixel[1] + target_img.shape[1]), (0,255,0), 1)
+        cv2.rectangle(inp_img_col,((int)(pixel[0] / scale_optimizer), (int)(pixel[1] / scale_optimizer)), ((int)(pixel[0] / scale_optimizer) + target_img.shape[0], (int)(pixel[1] / scale_optimizer) + target_img.shape[1]), (0,255,0), 1)
 
     # Shows input result image with rectangle
     cv2.imshow("Input Img", inp_img_col)
